60666ea6cc4b21d6a521c0d037c98899650d259f
C-F0x <MEiRa1n@outlook.com>
2025-06-08 10:36:13 +0800

    Initial commit

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2025 The Android Open Source Project
+# Copyright (C) 2025 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/twrp_gts10pwifi.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_gts10pwifi-user \
+    omni_gts10pwifi-userdebug \
+    omni_gts10pwifi-eng
---
+++ README.md
@@ -0,0 +1,2 @@
+# android_device_samsung_gts10pwifi
+
