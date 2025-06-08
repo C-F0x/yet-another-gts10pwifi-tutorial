bce46616cf8a18e29687dc79c6edcafb6c3a747b
C-F0x <132144679+C-F0x@users.noreply.github.com>
2025-06-09 00:13:36 +0800

    omni or twrp, lmao

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -9,6 +9,6 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/twrp_gts10pwifi.mk
 
 COMMON_LUNCH_CHOICES := \
-    omni_gts10pwifi-user \
-    omni_gts10pwifi-userdebug \
-    omni_gts10pwifi-eng
+    twrp_gts10pwifi-user \
+    twrp_gts10pwifi-userdebug \
+    twrp_gts10pwifi-eng
